e06e46d97355301266fcc15679965330ce1453c8
DuvanGG <60080389+DuvanGG@users.noreply.github.com>
2020-05-04 09:13:25 -0500

    Update editarUsuario.php

--- editarUsuario.php
+++ editarUsuario.php
@@ -1,26 +1,44 @@
+<?php
+
+
+
+
+require('conexion.php');
+ 
+$id = $_GET['id'];
+
+$sql = "SELECT * FROM  usuarios WHERE id = '$id'"; 
+
+$resultado = mysqli_query($conexion, $sql); 
+
+$resultado_bd = mysqli_fetch_all($resultado); #busca todo, esto devuelve un array 
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Editar usuario</title>
+    <title>Pagina principal</title>
     <link rel="stylesheet" href="estilos.css">
 </head>
 <body>
-<body>
-    <form method="POST" action="editar.php">
+
+
+
+    <?php foreach($resultado_bd as $datoUsuario): ?>
+       <form method="POST" action="editar.php">
         <p>Nombre</p>
-        <input type="text" name="nombre" placeholder="Ingresa nombre" class="input"><br>
+        <input type="text" name="nombre" value="<?php echo $datoUsuario[1]; ?>"><br>
         <p>Apellido</p> 
-        <input type="text" name="apellido" placeholder="Ingresa apellido" class="input"><br>
+        <input type="text" name="apellido" value="<?php echo $datoUsuario[2]; ?>"><br>
         <p>Direccion</p>
-        <input type="text" name="direccion" placeholder="Ingresa direccion" class="input"><br>
+        <input type="text" name="direccion" value="<?php echo $datoUsuario[3]; ?>"><br>
         <p>Telefono</p>
-        <input type="text" name="telefono" placeholder="Ingresa telefono" class="input"><br>
+        <input type="text" name="telefono" value="<?php echo $datoUsuario[4]; ?>"><br>
         <p>Correo</p>
-        <input type="text" name="correo" placeholder="Ingresa correo" class="input"><br>
+        <input type="text" name="correo" value="<?php echo $datoUsuario[5]; ?>"><br>
+         <input type="hidden" name="id" value="<?php echo $datoUsuario[0]; ?>"><br>
         <button type="submit" class="boton">Actualizar</button>
-    </form>
-    
-</body>
-</html>
+    </form><br>
+    <?php endforeach ?>
